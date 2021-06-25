c288ab7a56e089864d6c69cc7277f6e18a690394
Rachel Hutchinson <rachelhutchinson@Rachel-Hutchinson-MacBook-Pro.local>
2021-06-25 14:40:30 +0100

    Sample PDf generating and saving locally

--- server/workfllow/create-order.js
+++ server/workfllow/create-order.js
@@ -3,6 +3,57 @@ export const createOrder = async (order) => {
     console.log("CreateOrderFlow::", order);
 
     const s = 200;
+
+    // require dependencies
+    const PDFDocument = require('pdfkit');
+    const blobStream = require('blob-stream');
+
+    var doc = new PDFDocument();
+    //var stream = doc.pipe(blobStream());
+    var FileSystem = require('fs');
+    doc.pipe(FileSystem.createWriteStream('output.pdf'));
+    // draw some text
+    doc.fontSize(25).text('Here is some vector graphics...', 100, 80);
+
+    // some vector graphics
+    doc
+      .save()
+      .moveTo(100, 150)
+      .lineTo(100, 250)
+      .lineTo(200, 250)
+      .fill('#FF3300');
+
+    doc.circle(280, 200, 50).fill('#6600FF');
+
+    // an SVG path
+    doc
+      .scale(0.6)
+      .translate(470, 130)
+      .path('M 250,75 L 323,301 131,161 369,161 177,301 z')
+      .fill('red', 'even-odd')
+      .restore();
+
+    // and some justified text wrapped into columns
+    doc
+      .text('And here is some wrapped text...', 100, 300)
+      .font('Times-Roman', 13)
+      .moveDown() 
+      .text("lorem wrapped text skdfjflsdjfkl sjfslf jslfjsdfjsfjskdldfj ksdjdljskl fdsdhfdhfjsdhfksf hsdjfhsdkfhsdjk dfhjsdfh sdfhjk fdshfjsddj hsfjkdhfjksdf", {
+        width: 412,
+        align: 'justify',
+        indent: 30,
+        columns: 2,
+        height: 300,
+        ellipsis: true
+      });
+
+    // end and display the document in the iframe to the right
+    doc.end();
+    // stream.on('finish', function () {
+    //   console.log(stream.toBlobURL('application/pdf'));
+    // });
+    // console.log('doc', doc)
+
     // Validate the order
 
     // Generate address labels
