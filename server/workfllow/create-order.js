aa3e305c5ea4ee3491a9c9f1ebe66c8142e5316a
PauloLancao <paulolancao@hotmail.com>
2021-06-25 09:00:38 +0100

    Update create order workflow

--- server/workfllow/create-order.js
+++ server/workfllow/create-order.js
@@ -1,9 +1,8 @@
 export const createOrder = async (order) => {
-  const s = 200;
-
   return new Promise((resolve, reject) => {
     console.log("CreateOrderFlow::", order);
 
+    const s = 200;
     // Validate the order
 
     // Generate address labels
