145da45f5736b509f7a72fa03adeaf0a2a6e4399
Alexandra Herbert <Alexandra.Herbert@MadeTech.com>
2021-07-01 18:37:48 +0100

    AKH
    Adding some example code that works for adding a note to an order.
    Needs a lot of clean u:
    It seems to return an erropr even though it's successful
    I've put a global variable in there to keep a hold of the access token which is pretty bad
    There's some getting orders code that doesn't work
    Need to make sure that the params you pass in actually get passed to the request and it's not hard coded
    But I'm 1hr40mins past the time I should have left so I'm calling it.
    
    PEACE

--- server/endpoints.js
+++ server/endpoints.js
@@ -1,3 +1,6 @@
 // Endpoints exposed withing server.js used on server side clients
 export const ClickSit_CreateReturnLabels = "/clicksit/createreturnlabels";
 export const ClickSit_GetTrackingStatus = "/clicksit/gettrackingstatus";
+
+export const Shopify_GetOrders="/shopify/getOrders"
+export const Shopify_addOrderTrackingNote="/shopify/addTrackingNote"
